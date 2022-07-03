ad59d5546c2e17a8958164b90584152035a7e591
sabbirsam <samiubat14@gmail.com>
2022-07-03 17:55:18 +0600

    first commit

---
+++ src/components/DragDrop.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DragDrop(){
+    return (
+       <>
+       <div className="Picture"></div>
+       <div className="Board"></div>
+       </>
+      );
+}
+
+export default DragDrop;
